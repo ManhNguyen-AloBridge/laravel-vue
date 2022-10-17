fc7210399ce3633286dbbc098f547f6ccf8f19cd
manh.nguyen <manh.nguyen@alobridge.com>
2022-10-17 18:05:41 +0700

    first commit

---
+++ resources/js/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import productIndex from "../components/products/index.vue";
+import productCreate from "../components/products/create.vue";
+import productEdit from "../components/products/edit.vue";
+
+import notFound from "../components/notFound.vue";
+
+const routes = [
+    {
+        path: "/",
+        component: productIndex,
+    },
+    {
+        path: "/product/create",
+        component: productCreate,
+    },
+    {
+        path: "/product/edit/:id",
+        component: productEdit,
+        props: true,
+    },
+    {
+        path: "/:pathMath(.*)*",
+        component: notFound,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
+export default router;
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(ProductController::class)->prefix('product')->group(function () {
+    Route::get('/get-list', 'getAll');
+    Route::post('/create', 'store');
+    Route::get('/{id}', 'show');
+    Route::post('/{id}', 'update');
+});
